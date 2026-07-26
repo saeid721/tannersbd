492124a332390bb7799824ea6b611a0c2515726c
saeid721 <saeid721km@gmail.com>
2026-07-26 17:56:31 +0600

    Eligibility page update

--- src/pages/about/ProfilePage.jsx
+++ src/pages/about/ProfilePage.jsx
@@ -1,4 +1,4 @@
-// src/pages/HistoryPage.jsx
+// src/pages/about/ProfilePage.jsx
 import React, { useRef } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { motion, useInView } from 'framer-motion';
