b0fee4495fd5cb7ff64775585c3ec50ce3f1d0fb
saeid721 <saeid721km@gmail.com>
2026-07-26 18:31:59 +0600

    Remove obsolete mixed-decls from silenceDeprecations

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        silenceDeprecations: ['global-builtin', 'import', 'mixed-decls', 'color-functions']
+        silenceDeprecations: ['global-builtin', 'import', 'color-functions']
       }
     }
   }
